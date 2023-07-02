38e6de3d4a0799c2767ea1d47434864c49a54ef3
SANAT-01 <94187960+SANAT-01@users.noreply.github.com>
2023-07-02 22:30:19 +0530

    removing tensorflow

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
-import tensorflow as tf
+# import tensorflow as tf
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
